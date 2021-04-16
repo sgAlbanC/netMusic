0cb01cfe7925fa68809f861a6219d86a2c3083ed
oliver6565 <390200763@qq.com>
2021-04-16 12:15:20 +0800

    add files

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    登录组件..
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+// 单文件组件；加上scoped，防止与其他组件样式冲突
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App.vue'  // 根组件
+import router from './router'  // 路由
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
+// 整个项目入口文件
+
 new Vue({
-  router,
-  render: h => h(App)
+  router,   // 将路由挂载到了vue实例
+  render: h => h(App)   // 将根组件渲染到了页面
 }).$mount('#app')
